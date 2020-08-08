1c9e0b13cacb67ab002ea399a54cb07804032f03
Will Thompson <will.k.t@gmail.com>
2020-08-07 19:23:37 -0500

    fiddling around

--- kelloggrs/process.py
+++ kelloggrs/process.py
@@ -2,27 +2,52 @@
 
 import argparse
 import sys
+import os
 import logging
 import pytesseract
 import dask
 from datetime import datetime as dt
 from PIL import Image
 from pathlib import Path
-from typing import List, Set, Tuple
 from dask.distributed import Client
 
-def process_file(image_file: Path, logger) -> bool:
+
+def remove_text_files(in_path):
+    txt_files = list(in_path.rglob("*index-page-[0-9].txt"))
+    for txt_file in txt_files:
+        os.remove(txt_file)
+
+
+def process_file(image_file: Path, overwrite: bool) -> bool:
     try:
-        logger.info(f"processing file: {image_file.name}")
         outfile = Path(image_file.parent / f"{image_file.stem}.txt")
-        if not(outfile.is_file()):
+        if overwrite or not (outfile.is_file()):
             ocr_text = pytesseract.image_to_string(
-                Image.open(image_file), config=r"-l eng --psm 6 -c preserve_interword_spaces=1"
+                Image.open(image_file),
+                config=r"-l eng --psm 6 -c preserve_interword_spaces=1",
             )
             outfile.write_text(ocr_text)
         return True
     except:
         return False
+
+
+def make_logger(in_path: Path, verbose: bool) -> logging.Logger:
+    logger = logging.getLogger("process")
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    fh = logging.FileHandler(in_path / "process.log")
+    fh.setFormatter(formatter)
+    fh.setLevel(logging.INFO)
+    logger.addHandler(fh)
+    if verbose:
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        ch.setLevel(logging.INFO)
+        logger.addHandler(ch)
+    return logger
 
 
 def main():
@@ -31,60 +56,58 @@ def main():
     parser.add_argument("in_path", help="the path to the input image files")
     parser.add_argument("page_nums", help="the page numbers to process")
     parser.add_argument("n_workers", help="number of workers to use")
-
     parser.add_argument(
         "-v", "--verbose", help="increase verbosity", action="store_true"
     )
-
+    parser.add_argument(
+        "-f", "--force", help="over-write existing files", action="store_true"
+    )
     if len(sys.argv[1:]) == 0:
         parser.print_usage()
         parser.exit()
-
     args = parser.parse_args()
 
-    # set up logger
-    logger = logging.getLogger("process")
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    fh = logging.FileHandler(Path(args.in_path) / "process.log")
-    fh.setFormatter(formatter)
-    fh.setLevel(logging.INFO)
-    logger.addHandler(fh)
-    if args.verbose:
-        ch = logging.StreamHandler()
-        ch.setFormatter(formatter)
-        ch.setLevel(logging.INFO)
-        logger.addHandler(ch)
-
+    # determine which pages to process
     if args.page_nums in ["all", "All", "ALL"]:
         page_nums = None
     else:
         page_nums = {int(n) for n in args.page_nums.split(",")}
 
     in_path = Path(args.in_path)
-
     n_workers = int(args.n_workers)
+    over_write = args.force
 
-    dask.config.set(scheduler="multiprocessing")
-    dask.config.set(n_workers=n_workers)
+    # make the logger
+    logger = make_logger(in_path, args.verbose)
 
-    logger.info(f"processing subdirectories in path: {in_path}")
+    logger.info(f"processing image files in path: {in_path}")
+    logger.info(f"page numbers to process: {args.page_nums}")
+    logger.info(f"using number of workers: {n_workers}")
+    logger.info(f"over-write existing files: {over_write}")
+
+    client = Client(n_workers=n_workers)
+    # dask.config.set(scheduler="multiprocessing")
+    # dask.config.set(n_workers=n_workers)
+
     image_files = list(in_path.rglob("*index-page-[0-9].png"))
-
     process_tasks = []
     start = dt.now()
+    # sum_result = 0
     for image_file in image_files:
         if page_nums:
             curr_num = int(image_file.stem.split("-")[-1])
             if curr_num not in page_nums:
                 continue
-        process_tasks.append(dask.delayed(process_file)(image_file, logger))
+        # sum_result += process_file(image_file, over_write)
+        process_tasks.append(dask.delayed(process_file)(image_file, over_write))
     result = dask.delayed(sum)(process_tasks)
     sum_result = dask.compute(result)[0]
     end = dt.now()
-    logger.info(f"{in_path.name}: {sum_result} pages successfully processed in {end-start} seconds")
+    logger.info(
+        f"{in_path.name}: {sum_result} pages successfully processed in {end-start} seconds"
+    )
+
+    client.close()
     sys.exit(0)
 
 
--- tests/test_process.py
+++ tests/test_process.py
@@ -1,7 +1,5 @@
 import pytest
 import os
-import logging
-import fitz
 from pathlib import Path
 from kelloggrs import process
 
@@ -11,21 +9,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 @pytest.fixture
 def process_logger(config):
-    logger = logging.getLogger("process")
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-
-    fh = logging.FileHandler(f"{dir_path}/{config['in_path']}/process.log")
-    fh.setFormatter(formatter)
-    fh.setLevel(logging.INFO)
-    logger.addHandler(fh)
-    ch = logging.StreamHandler()
-    ch.setFormatter(formatter)
-    ch.setLevel(logging.INFO)
-    logger.addHandler(ch)
-    return logger
+    return process.make_logger(config["in_path"], True)
 
 
 def test_process_script(script_runner, config):
@@ -36,6 +20,5 @@ def test_process_script(script_runner, config):
     assert ret.stdout != ""
     assert ret.stderr == ""
 
-    ret = script_runner.run("process_pages", "-v", f"{in_path}", "1,2,3", "8")
+    ret = script_runner.run("process_pages", "-v", "-f", f"{in_path}", "1,2,3", "8")
     assert ret.success
-
